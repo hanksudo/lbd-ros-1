0a449cb1bc2df3e28e48107a4e753de79dc2f7b0
Hank Wang <drapho@gmail.com>
2025-09-03 02:05:03 +0900

    [rust] service

---
+++ src/my_robot_tutorials/add_two_ints_rust/src/add_two_ints_client.rs
@@ -0,0 +1,33 @@
+use rosrust;
+use std::time;
+use env_logger::Builder;
+use log::info;
+use std::io::Write;
+
+fn main() {
+    Builder::new()
+        .format(|buf, record| {
+            let timestamp = std::time::SystemTime::now()
+                .duration_since(std::time::UNIX_EPOCH)
+                .unwrap()
+                .as_secs_f64();
+            writeln!(buf, "[INFO] [{:.9}]: {}", timestamp, record.args())
+        })
+        .filter_level(log::LevelFilter::Info)
+        .init(); 
+    rosrust::init("add_two_ints_client");
+
+    rosrust::wait_for_service("/add_two_ints", Some(time::Duration::from_secs(10)))
+        .expect("Service not available");
+
+    let client = rosrust::client::<rosrust_msg::rospy_tutorials::AddTwoInts>("/add_two_ints")
+        .expect("Failed to create client");
+
+    match client.req(&rosrust_msg::rospy_tutorials::AddTwoIntsReq { a: 5, b: 6 }) {
+        Ok(Ok(response)) => info!("Returned sum is {}", response.sum),
+        Ok(Err(e)) => {
+            info!("Service returned an error: {:?}", e);
+        }
+        Err(e) => info!("Failed to call service: {:?}", e),
+    }
+}
---
+++ src/my_robot_tutorials/add_two_ints_rust/src/add_two_ints_server.rs
@@ -0,0 +1,33 @@
+use rosrust;
+use env_logger::Builder;
+use std::io::Write;
+
+fn setup_logger() {
+    Builder::new()
+        .format(|buf, record| {
+            let timestamp = std::time::SystemTime::now()
+                .duration_since(std::time::UNIX_EPOCH)
+                .unwrap()
+                .as_secs_f64();
+            writeln!(buf, "[INFO] [{:.9}]: {}", timestamp, record.args())
+        })
+        .filter_level(log::LevelFilter::Info)
+        .init();
+}
+
+fn main() {
+	setup_logger();
+
+    rosrust::init("add_two_ints_server");
+    let _service = rosrust::service::<rosrust_msg::rospy_tutorials::AddTwoInts, _>("add_two_ints", move |req| {
+		let sum = req.a + req.b;
+		rosrust::ros_info!("Received request: {} + {} = {}", req.a, req.b, sum);
+
+		Ok(rosrust_msg::rospy_tutorials::AddTwoIntsRes { sum })
+	})
+        .expect("Failed to create service");
+
+    rosrust::ros_info!("Add Two Ints server ready");
+
+    rosrust::spin();
+}
