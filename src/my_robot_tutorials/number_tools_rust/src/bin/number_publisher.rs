19aa85d25cb727ebf32c85dd898caf974c869fd3
Hank Wang <drapho@gmail.com>
2025-11-28 17:40:17 +0900

    make it anonymous

--- src/my_robot_tutorials/number_tools_rust/src/bin/number_publisher.rs
+++ src/my_robot_tutorials/number_tools_rust/src/bin/number_publisher.rs
@@ -1,17 +1,21 @@
 use rosrust;
 use rosrust_msg::std_msgs::Int64;
 use log::{error, info};
+use std::process;
 
 fn main() {
-    env_logger::init();
+    env_logger::Builder::from_default_env()
+        .filter_level(log::LevelFilter::Info)
+        .init();
 
-    rosrust::init("number_publisher_rust");
+    let node_name = format!("number_publisher_{}", process::id());
+    rosrust::init(&node_name);
 
     let publisher = rosrust::publish::<Int64>("/number", 10).expect("Failed to create publisher");
     let rate = rosrust::rate(1.0);
 
     let mut value: i64 = 0;
-    info!("number_publisher_rust started");
+    info!("number_publisher started");
 
     while rosrust::is_ok() {
         let mut msg = Int64::default();
